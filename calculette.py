d939045afd8e046b4edcc83ae36294dd0c888ec3
NathanESD <max_949@hotmail.com>
2016-02-29 09:54:43 -0500

    nouvelle calculette

---
+++ calculette.py
@@ -0,0 +1,22 @@
+#calculette
+
+
+#Valeurs
+
+nb1 = int(input('Premier nombre : '))
+nb2 = int(input('Deuxieme nombre : '))
+op = raw_input('operateur : ')
+
+#calcul
+
+if (op == '*'):
+  print (nb1 * nb2)
+
+elif (op == '-'):
+  print (nb1 - nb2)
+
+elif (op == '/'):
+  print (nb1 / nb2)
+
+elif (op == '+'):
+  print (nb1 + nb2)
